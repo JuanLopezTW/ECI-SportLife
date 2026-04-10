05401731c86bddd485b6afdde6cd35bc0d16f35c
Juan Lopez <juan.lbarrera@mail.escuelaing.edu.co>
2026-04-09 19:37:57 -0500

    cambio para separar la pipe line en varios jobs y modificacion al ApliccationTest

--- src/test/java/com/dosw/sportlife/sportlife/SportlifeApplicationTests.java
+++ src/test/java/com/dosw/sportlife/sportlife/SportlifeApplicationTests.java
@@ -1,9 +1,7 @@
 package com.dosw.sportlife.sportlife;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SportlifeApplicationTests {
 
 	@Test
